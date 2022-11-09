0fa5489970fe6808d9b21c98661fbf375f500e41
root <root@Ubuntu22.04>
2022-11-09 23:12:19 +0800

    modified for a cleaner look for CSS

--- create.php
+++ create.php
@@ -5,10 +5,19 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Student Registration Form</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<style>
+		.center{
+			text-align: center;
+		}
+		table{
+			margin-left: auto;
+			margin-right: auto;
+		}
+	</style>
 </head>
 <body class="p-3 mb-2 bg-dark text-white">
-	<h1 style="text-align:center;">Student Registration Form</h1>
-	<table style="margin-left:auto;margin-right:auto;">
+	<h1 class="center">Student Registration Form</h1>
+	<table>
 	<form action="register.php" method="post">	
 		<tr>
 			<td>
